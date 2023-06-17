fe027479513214c2efaa1e15c791cbf1e919cdcc
guillermoyano <guillermoyano2002@gmail.com>
2023-06-17 02:51:17 -0300

    commit form juegos

---
+++ JS/tipodejuego.js
@@ -0,0 +1,11 @@
+document.getElementById("tipo-juego").addEventListener("change", function() {
+    var selectedOption = this.value;
+    // Realizar acciones basadas en la opción seleccionada
+    if (selectedOption === "tranquilos") {
+      // Código para juegos tranquilos
+    } else if (selectedOption === "ruidosos") {
+      // Código para juegos ruidosos
+    } else if (selectedOption === "para-niños") {
+      // Código para juegos para niños
+    }
+  });
